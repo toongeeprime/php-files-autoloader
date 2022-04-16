ac776c96f33cc7328272f33bc53d3a5038359404
Tunji Makanjuola <39152476+toongeeprime@users.noreply.github.com>
2022-04-16 19:14:05 +0100

    Update php-files-autoloader.php

--- php-files-autoloader.php
+++ php-files-autoloader.php
@@ -4,27 +4,27 @@
  *	@author  ToongeePrime <toongeeprime@gmail.com>
  *
  *	GET PHP FILES CONTAINED IN AN ARRAY OF DIRECTORIES
- *  This file should be in the root directory
+ *	This file should be in the root directory
  */
 
 
 // The Directories Array:
-$directories	=	[ 'classes', 'includes', 'directory3', 'otherdirectory' ];
+$directories = [ 'classes', 'includes', 'directory3', 'otherdirectory' ];
 
 foreach( $directories as $dir ) {
 
-  $folder	=	dirname( __FILE__ ) . '/' . $dir . '/'; // Full path to each folder
+	$folder = dirname( __FILE__ ) . '/' . $dir . '/'; // Full path to each folder
 
 	// Get all files contained in folder
 	$files	=	scandir( $folder );
 
 	foreach( $files as $file ) {
-  $path	=	$folder . $file; // Full path to each file
+		$path = $folder . $file; // Full path to each file
 
-	// Ensure file $path is not a directory and
-	// Check that they have the php extension
-	if ( ! is_dir( $path ) && pathinfo( $path )[ 'extension' ] == 'php' )
-		require_once $path;
+		// Ensure file $path is not a directory and
+		// Check that they have the php extension
+		if ( ! is_dir( $path ) && pathinfo( $path )[ 'extension' ] == 'php' )
+			require_once $path;
 	}
 
 }
